7c070ab9ef39caca13918a4e4a8d625e4344cbd6
sachin931350 <sachin931350@gmail.com>
2016-03-18 18:32:28 -0700

    drawer with two fragments

---
+++ app/src/main/java/com/android/wrokshop/drawer/Home.java
@@ -0,0 +1,18 @@
+package com.android.wrokshop.drawer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by A on 3/18/2016.
+ */
+public class Home extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home,container,false);
+    }
+}
